1f96f7cdcab7cd7088b4127cdd05a2a323c210d2
ruben <ruben@302.nl>
2025-09-22 19:02:44 +0200

    Refactor AppShellClient and Navbar components for improved readability and consistency

--- frontend/src/components/layout/AppShellClient.tsx
+++ frontend/src/components/layout/AppShellClient.tsx
@@ -1,32 +1,32 @@
 "use client";
 import React from "react";
-import {AppShell, Burger} from "@mantine/core";
-import {useDisclosure} from "@mantine/hooks";
-import {Navbar} from "@frontend/components/layout/Navbar";
-import {ChronoIcon} from "@frontend/components/icons/ChronoIcon";
+import { AppShell, Burger } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { Navbar } from "@frontend/components/layout/Navbar";
+import { ChronoIcon } from "@frontend/components/icons/ChronoIcon";
 
 export default function AppShellClient({
-                                           children,
-                                       }: {
+    children,
+}: {
     children: React.ReactNode;
 }) {
-    const [opened, {toggle}] = useDisclosure(false);
+    const [opened, { toggle }] = useDisclosure(false);
 
     return (
         <AppShell
             padding="md"
-            header={{height: 60}}
-            navbar={{width: 300, breakpoint: "sm", collapsed: {mobile: !opened}}}
+            header={{ height: 60 }}
+            navbar={{ width: 300, breakpoint: "sm", collapsed: { mobile: !opened } }}
         >
             <AppShell.Header>
-                <div style={{display: "flex", alignItems: "center", gap: 12}}>
-                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm"/>
-                    <ChronoIcon width={80} height={80}/>
+                <div style={{ display: "flex", alignItems: "center", gap: 12 }}>
+                    <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+                    <ChronoIcon width={60} height={60} />
                 </div>
             </AppShell.Header>
 
             <AppShell.Navbar>
-                <Navbar/>
+                <Navbar />
             </AppShell.Navbar>
             <AppShell.Main>{children}</AppShell.Main>
         </AppShell>
--- frontend/src/components/layout/Navbar.tsx
+++ frontend/src/components/layout/Navbar.tsx
@@ -1,3 +1,10 @@
+import {IconHome2} from "@tabler/icons-react";
+import {NavLink} from "@mantine/core";
+
 export function Navbar() {
-  return <div>je moder</div>;
+  return <NavLink
+    href="/home"
+    label="Home"
+    leftSection={<IconHome2 size={16} stroke={1.5}/>}
+  />;
 }
